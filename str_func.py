69fcee7a328b5ce26017748fe8cbee4464b65d95
Sergey <smirsand@mail.ru>
2023-05-21 22:15:53 +0300

    "Добавлена функция которая принимает текст и возвращает слова с большой буквы. Добавлен догстринг в первую функцию"

--- str_func.py
+++ str_func.py
@@ -1,7 +1,19 @@
 def capital_letters_of_words(text):
+    '''
+    Принимает слова с маленькими буквами и возвращает слова с большими буквами
+    '''
     text = text.upper()
+    return text
+
+def first_letter(text_):
+    '''
+    Принимает слова с маленькими буквами и возавращает слова с большой буквы
+    '''
+    text = text_.title()
     return text
 
 
 text = "слово"
+text_ = "второе слово"
 capital_letters_of_words(text)
+first_letter(text_)
